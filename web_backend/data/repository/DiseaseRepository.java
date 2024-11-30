0b9fd2b73e52470d122b9cbc10d696ba245085e0
Riti Shrivastav <134117479+riti8517@users.noreply.github.com>
2024-11-29 21:49:01 -0800

    Delete web_backend directory

--- web_backend/data/repository/DiseaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.riti.web_backend.data.repository;
-
-import com.riti.web_backend.data.entity.DiseaseDto;
-
-public interface DiseaseRepository {
-   DiseaseDto getDescriptionByDisease(String diseaseName);
-}
